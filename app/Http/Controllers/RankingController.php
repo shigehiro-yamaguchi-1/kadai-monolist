d7bd426582c258790b1fcd829f800bd7dfc9a92d
tester <erta.6224@gmail.com>
2017-11-06 05:47:20 +0000

    have ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -17,6 +17,18 @@ class RankingController extends Controller
 
         return view('ranking.want', [
             'items' => $items,
+            'type' => 'Wants',
         ]);
     }
+
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id')->orderBy('count', 'DESC')->take(10)->get();
+
+        return view('ranking.have', [
+            'items' => $items,
+            'type' => 'Haves',
+        ]);
+    }
+
 }
